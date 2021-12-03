5f460cca1a9396216d90f8f283b57f92586c3f71
Daniel Pecos Martinez <me@danielpecos.com>
2021-12-03 20:10:58 +0100

    Puzzle 06

---
+++ src/bin/puzzle_06/main.rs
@@ -0,0 +1,129 @@
+use utils::files::read_lines;
+
+fn count_bits(lines: &Vec<String>) -> Vec<i16> {
+  // let mut counters = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let mut counters = vec![];
+  for line in lines {
+    for (i, c) in line.chars().enumerate() {
+      if i >= counters.len() {
+        counters.push(0);
+      }
+      match c {
+        '0' => counters[i] -= 1,
+        '1' => counters[i] += 1,
+        _ => (),
+      }
+    }
+  }
+
+  counters
+}
+
+fn convert_counter_to_binary_string(counter: Vec<i16>) -> String {
+  println!("{:?}", counter);
+  let mut binary_string = String::new();
+  for i in counter {
+    let current = if i < 0i16 {
+      '0'
+    } else if i == 0i16 {
+      '='
+    } else {
+      '1'
+    };
+    binary_string.push(current);
+  }
+  binary_string
+}
+
+fn negate_binary_string(input: &String) -> String {
+  let mut output = String::new();
+  for c in input.chars() {
+    match c {
+      '0' => output.push('1'),
+      '1' => output.push('0'),
+      '=' => output.push('='),
+      _ => (),
+    }
+  }
+  output
+}
+
+fn convert_binary_string_to_number(input: String) -> i32 {
+  let mut output = 0i32;
+  for (i, c) in input.chars().rev().enumerate() {
+    let current = if c == '1' { 1 } else { 0 };
+    output += current * 2i32.pow(i as u32);
+  }
+  output
+}
+
+fn filter_lines(path: &str, negate: bool) -> String {
+  let mut result = vec![];
+
+  if let Ok(lines) = read_lines(path) {
+    for line_result in lines {
+      if let Ok(line) = line_result {
+        result.push(line);
+      }
+    }
+  }
+
+  let mut criteria_pos = 0;
+
+  while result.len() > 1 {
+    let counters = count_bits(&result);
+    let most_common_bits = convert_counter_to_binary_string(counters);
+
+    let criteria_bits = if negate {
+      negate_binary_string(&most_common_bits)
+    } else {
+      most_common_bits
+    };
+
+    let c = criteria_bits.chars().nth(criteria_pos).unwrap();
+
+    let mut criteria = c;
+    if c == '=' {
+      criteria = if negate { '0' } else { '1' };
+    }
+    // if result.len() == 2 {
+    //   criteria = '1'
+    // }
+
+    let mut filtered = vec![];
+    for line in result.iter() {
+      if criteria == line.chars().nth(criteria_pos).unwrap() {
+        filtered.push(line.clone());
+      }
+    }
+    println!(
+      "{:?} {} {} - {:?} {:?}",
+      criteria_bits, criteria_pos, criteria, result, filtered
+    );
+    result = filtered;
+
+    criteria_pos += 1;
+  }
+
+  result[0].clone()
+}
+
+fn main() {
+  // let path = "./src/bin/puzzle_05/test_data.txt";
+  let path = "./src/bin/puzzle_05/data.txt";
+
+  let oxygen_generation_rate_bin = filter_lines(&path, false);
+  println!(
+    "Oxygen generation rate (bin) {:?}",
+    oxygen_generation_rate_bin
+  );
+  let oxygen_generation_rate = convert_binary_string_to_number(oxygen_generation_rate_bin);
+  println!("Oxygen generation rate {}", oxygen_generation_rate);
+
+  let co2_scrubber_rate_bin = filter_lines(&path, true);
+  println!("CO2 scrubber rate (bin) {:?}", co2_scrubber_rate_bin);
+  let co2_scrubber_rate = convert_binary_string_to_number(co2_scrubber_rate_bin);
+  println!("CO2 scrubber rate {}", co2_scrubber_rate);
+
+  println!("Result: {}", oxygen_generation_rate * co2_scrubber_rate);
+}
